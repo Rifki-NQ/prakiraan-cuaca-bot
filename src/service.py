ca45e4caa9eccb128e84c7e3a637df58af680335
Rifki-NQ <rifqinf950@gmail.com>
2026-07-08 16:47:30 +0700

    refactor(service): add timedelta as optional paramater in _setup_datetime
    also add new method get_tomorrow_weather_forecast

--- src/service.py
+++ src/service.py
@@ -1,6 +1,6 @@
 import asyncio
 from collections.abc import AsyncIterable
-from datetime import datetime
+from datetime import datetime, timedelta
 from src.models.dt_model import DatetimeModel
 from src.models.domain_model import ForecastModel
 from src.models.protocols import QueryBuilderProtocol
@@ -13,11 +13,26 @@ class AppService:
     async def get_today_weather_forecast(self) -> AsyncIterable[ForecastModel]:
         dt = self._setup_datetime()
         today_dt_range = (dt.current_datetime_start, dt.current_datetime_end)
-        rows = await self.query.get_forecast_by_range(today_dt_range)
+        return self._yield_forecast(today_dt_range)
+
+    async def get_tomorrow_weather_forecast(self) -> AsyncIterable[ForecastModel]:
+        dt = self._setup_datetime(timedelta(days=1))
+        tomorrow_dt_range = (dt.current_datetime_start, dt.current_datetime_end)
+        return self._yield_forecast(tomorrow_dt_range)
+
+    async def _yield_forecast(
+        self, dt_range: tuple[datetime, datetime]
+    ) -> AsyncIterable[ForecastModel]:
+        rows = await self.query.get_forecast_by_range(dt_range)
         async for row in rows:
             yield ForecastModel(**row._mapping)  # pyright: ignore[reportPrivateUsage]
             await asyncio.sleep(0)
 
-    def _setup_datetime(self) -> DatetimeModel:
-        """Methods that need dt attribute need to get through here"""
-        return DatetimeModel(datetime.now())
+    def _setup_datetime(self, timedelta: timedelta | None = None) -> DatetimeModel:
+        """
+        Return DatetimeModel with datetime.now(),
+        add datetime.now() with timedelta if not None.
+        """
+        if timedelta is None:
+            return DatetimeModel(datetime.now())
+        return DatetimeModel(datetime.now() + timedelta)
