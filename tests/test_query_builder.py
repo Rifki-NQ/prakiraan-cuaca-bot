6e48c7691e25e73c47aca713d449bd825f0cac0c
Rifki-NQ <rifqinf950@gmail.com>
2026-07-10 15:52:32 +0700

    test(query_builder): fix test case test_invalid_datetime_range

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -1,14 +1,14 @@
 import pytest
-from unittest.mock import MagicMock
 from datetime import datetime
 from src.query import QueryBuilder
 from src.exceptions import InvalidDatetimeRangeError
 
 
-@pytest.mark.parametrize("start_dt", [datetime(2020, 2, 2), datetime(2020, 2, 3)])
+@pytest.mark.parametrize("start_dt", [datetime(2020, 2, 2, 1, 1, 1), datetime(2020, 2, 3)])
 async def test_invalid_datetime_range(start_dt: datetime) -> None:
+    """InvalidDatetimeRangeError should be raised when start_dt is greater than end_dt"""
     end_dt = datetime(2020, 2, 2)
-    obj = QueryBuilder(MagicMock(), MagicMock())
+    obj = QueryBuilder()
     datetime_range = (start_dt, end_dt)
     with pytest.raises(InvalidDatetimeRangeError) as exc_info:
         await obj.get_forecast_by_range(datetime_range)
