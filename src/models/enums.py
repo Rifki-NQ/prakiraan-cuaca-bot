ad8ab1c135345547c7bd0f104665706851e984d0
Rifki-NQ <rifqinf950@gmail.com>
2026-08-07 16:45:45 +0700

    feat(enums): add enums classes to the project

---
+++ src/models/enums.py
@@ -0,0 +1,37 @@
+from enum import Enum, auto
+
+
+class Commands(Enum):
+    START = "/start"
+    LOCATION = "/location"
+    INPUT = "/input"
+    TODAY = "/today"
+    TOMORROW = "/tomorrow"
+
+
+class BotAction(Enum):
+    SHOW_INTRO = auto()
+    ASK_CITY_OR_REGENCY = auto()
+    SHOW_CITY_OR_REGENCY_OPTIONS = auto()
+    ASK_SUBDISTRICT = auto()
+    SHOW_SUBDISTRICT_OPTIONS = auto()
+    ASK_VILLAGE = auto()
+    SHOW_VILLAGE_OPTIONS = auto()
+    SHOW_USER_CURRENT_LOCATION = auto()
+    SHOW_WELCOME_BACK_INTRO = auto()
+    SHOW_TODAY_FORECASTS = auto()
+    SHOW_TOMORROW_FORECASTS = auto()
+    TELLS_USER_TO_SET_LOCATION = auto()
+    TELLS_USER_NO_NEED_FOR_INPUT = auto()
+    TELLS_USER_TO_FINISH_SET_LOCATION = auto()
+    RECEIVE_INPUT_FOR_CITY_OR_REGENCY = auto()
+    RECEIVE_INPUT_FOR_SUBDISTRICT = auto()
+    RECEIVE_INPUT_FOR_VILLAGE = auto()
+
+
+class UserLocationState(Enum):
+    NO_STATE = auto()
+    NO_CITY_OR_REGENCY = auto()
+    NO_SUBDISTRICT = auto()
+    NO_VILLAGE = auto()
+    COMPLETE = auto()
