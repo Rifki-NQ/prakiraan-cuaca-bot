8d63269889d9a07f26d1e5064eef09d8e2dff5e4
Rifki-NQ <rifqinf950@gmail.com>
2026-08-17 23:40:30 +0700

    tests: add new pytest marker in pyproject.toml and add test helper function get_tables_name

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,8 +1,18 @@
-from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import AsyncConnection
+from sqlalchemy import MetaData, inspect
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 
-async def drop_all_tables(conn: AsyncConnection) -> None:
+async def drop_all_tables(engine: AsyncEngine) -> None:
+    """Drop all tables of a database."""
     metadata = MetaData()
-    await conn.run_sync(metadata.reflect)
-    await conn.run_sync(metadata.drop_all)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.reflect)
+        await conn.run_sync(metadata.drop_all)
+
+
+async def get_tables_name(engine: AsyncEngine) -> list[str]:
+    """Return list of table names from a database."""
+    async with engine.connect() as conn:
+        return await conn.run_sync(
+            lambda sync_conn: inspect(sync_conn).get_table_names()
+        )
